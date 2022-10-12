8a55b20d7160ffc8ddc386ca10ec1eb6f5839f6e
inPhoenix <spiderk@gmail.com>
2022-10-12 16:03:01 -0300

    :sunrise_over_mountains: finishing plugin cached mechanism

--- src/plugins/unpkg-path-plugin.tsx
+++ src/plugins/unpkg-path-plugin.tsx
@@ -1,12 +1,16 @@
 import * as esbuild from "esbuild-wasm"
 import axios from "axios"
+import localforage from "localforage"
+
+const fileCache = localforage.createInstance({
+  name: "filecache",
+})
 
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResole", args)
         if (args.path === "index.js") {
           return { path: args.path, namespace: "a" }
         }
@@ -28,8 +32,12 @@ export const unpkgPathPlugin = () => {
       })
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args)
-
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        )
+        if (cachedResult) {
+          return cachedResult
+        }
         if (args.path === "index.js") {
           return {
             loader: "jsx",
@@ -40,11 +48,13 @@ export const unpkgPathPlugin = () => {
           }
         }
         const { data, request } = await axios.get(args.path)
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         }
+        await fileCache.setItem(args.path, result)
+        return result
       })
     },
   }
