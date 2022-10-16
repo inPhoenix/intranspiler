fa2db7790a7f9f775777ba9b0fdce59d61b59872
inPhoenix <spiderk@gmail.com>
2022-10-16 14:04:06 -0300

    :night_with_stars: adding error

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,13 @@ const App = () => {
         <div id="root"></div>
         <script>
           window.addEventListener('message', (event) => {
-            eval(event.data)
+            try {
+            eval(event.data)  
+            } catch(err) {
+              console.log('%c error', 'background: white; color: red', err);
+              const root = document.querySelector('#root')
+              root.innerHTML = '<div style="color: red">' + err + '</div>'
+            }
           }, false)
         </script>
       </body>  
